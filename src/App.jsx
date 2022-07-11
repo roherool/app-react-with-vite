7ec0091cecbb5f72f70e813f38d8ce2e070eab60
Roberto <roherool@hotmail.com>
2022-07-11 12:47:08 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./components/Header"
+import { Post } from "./pages/Post"
+
+import "./styles/global.css"
+
+export function App() {
+
+  return (
+    <>
+      <Header />
+      <Post />
+    </>
+  )
+}
+
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import styles from "./styles.module.css";
+
+import igniteLogo from "../../assets/ignite-logo.svg";
+
+export function Header() {
+  return (
+    <header className={styles.container}>
+      <img src={igniteLogo} alt="Logotipo do Ignite" />
+      <h1>Feed</h1>
+    </header>
+  );
+}
