04e336f985cab0742fa9b9d6c5cddc34484f3b1a
Roberto <roherool@hotmail.com>
2022-07-12 08:57:38 -0300

    Created the styling of the comment component

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -1,7 +1,40 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+
 import styles from "./styles.module.css";
 
 export function Comment() {
   return (
-    <div>Comentário</div>
+    <div className={styles.container}>
+      <img src="https://github.com/roherool.png" alt="" className={styles.avatar} />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Roberto Oliveira</strong>
+              <time
+                title="11 de Maio às 08:13h"
+                dateTime="2022-05-11 08:13:30"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp size={20} />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
   );
 }
