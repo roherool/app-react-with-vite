49005b27cad563b0e076c9348396108024cb5800
Roberto <roherool@hotmail.com>
2022-07-13 11:03:13 -0300

    Refactoring the comment likes function

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -13,7 +13,9 @@ export function Comment({ content, onDeleteComment }) {
   }
 
   const handleLikeComment = () => {
-    setLikeCount(likeCount + 1);
+    setLikeCount((state) => {
+      return state + 1
+    });
   }
 
   return (
